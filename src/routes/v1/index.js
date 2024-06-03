21bb65eb1d43cf8f16a7bdb6d635c88ad3546012
bhusandas <sonubhusan98@gmail.com>
2024-06-03 17:58:43 +0530

    Added middleware to the auth service

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const UserController = require("../../controllers/user-controller")
+const {AuthRequestValidators} = require('../../middlewares/index')
 
 const router = express.Router();
 
-router.post('/signup',UserController.create);
-router.post('/signIn',UserController.signIn);
+router.post('/signup',AuthRequestValidators.validateUserAuth,
+UserController.create);
+router.post('/signIn',AuthRequestValidators.validateUserAuth,
+UserController.signIn);
 
 module.exports = router;
